29e289d50f29217bf40a35012186bb4d79911721
Alex-Maldonado <alexandermaldonadosoto@gmail.com>
2024-01-05 07:34:26 -0600

    feat(todos): :construction: Añadi una nueva libreria llamada uuidv4 que sirve para crear id unicos, y añadi las funciones necesarias para eliminar, identificar y tachar todos

--- src/assets/componentes/Todos.jsx
+++ src/assets/componentes/Todos.jsx
@@ -2,7 +2,6 @@ import {
   Button,
   Checkbox,
   Container,
-  Divider,
   IconButton,
   List,
   ListItem,
@@ -13,26 +12,53 @@ import { Theme } from "../Theme";
 import Brightness1OutlinedIcon from "@mui/icons-material/Brightness1Outlined";
 import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
 import CloseIcon from "@mui/icons-material/Close";
+import { v4 as uuidv4 } from "uuid";
 
 export default function Todos() {
-  const [todos, setTodos] = React.useState(["hola", "chau"]);
-  const [check, setCheck] = React.useState([true, false]);
+  const [todos, setTodos] = React.useState([]);
+  const [check, setCheck] = React.useState([]);
 
   const handleAddTodos = () => {
-    const newTodos = {
-      Name: Math.random(),
-      id: todos.length - 1,
+    const newTodo = {
+      name: Math.random(),
+      id: uuidv4(),
+      check: true,
     };
-    setTodos([...todos, newTodos]);
-    setCheck([...check, false]);
+    const newCheck = {
+      id : newTodo.id,
+      value: false
+    }
+    setTodos([...todos, newTodo]);
+    setCheck([...check, newCheck]);
+  };
+  ///////uuidv4 es una libreria que genera un id aleatorio
+  //////newTodos lo que hace es crear un objeto con propiedades
+  //////Luego setTodos selecciona todos los todos ya existentes y agrega el nuevo objeto con los valores correspondientes
+
+  const deleteTodos = (id) => {
+    const newTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(newTodos);
   };
 
-  function checkBox(id, checked) {
-    console.log(id, checked);
+  ////Lo que hace es filtrar todos los todos que no coincidan con el id y crea todos menos el que coincida
+
+  const handleCheck = (id) => {
+    const newTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        return {
+          ...todo,
+          check: !todo.check,
+        };
+      }
+      return todo;
+    });
+    setTodos(newTodos);
   }
+//////Lo que hace es cambiar el valor de check del todo que coincida con el id y vuelve a crear todos
 
   const listTodos = todos.map((todo) => (
     <ListItem
+      key={todo.id}
       sx={{
         display: "flex",
         justifyContent: "space-between",
@@ -41,21 +67,18 @@ export default function Todos() {
     >
       <Checkbox
         color="secondary"
-        checked={check[todo.id]}
-        onChange={(e) => {
-          setCheck([
-            ...check.slice(0, todo.id),
-            e.target.checked,
-            ...check.slice(todo.id + 1),
-          ]);
-          
-          console.log(e.target.checked, todo.id);
+        checked={todo.check}
+        onChange={() => {
+          handleCheck(todo.id);
         }}
         icon={<Brightness1OutlinedIcon />}
         checkedIcon={<CheckCircleOutlineIcon />}
       />
-      <ListItemText primary={todo.id} />
-      <IconButton color="secondary">
+      <ListItemText
+        primary={todo.id}
+        sx={{ textDecoration: todo.check ? "line-through" : "none" }}
+      />
+      <IconButton color="secondary" onClick={() => deleteTodos(todo.id)}>
         <CloseIcon label="clickable" />
       </IconButton>
     </ListItem>
